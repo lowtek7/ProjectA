ddbc41d489ed6d5615c2528e22a4f299bec0ae27
Amber <lowtek7@gmail.com>
2023-02-10 00:12:21 +0900

    Spawner 기능 수정

--- Assets/Scripts/Service/Spawner.cs
+++ Assets/Scripts/Service/Spawner.cs
@@ -2,7 +2,28 @@ using System;
 using BlitzEcs;
 
 namespace Game.Service
-{	
+{
+	public class SpawnCommander
+	{
+		private Entity entity;
+		
+		public SpawnCommander(World world)
+		{
+			entity = world.Spawn();
+		}
+
+		public SpawnCommander Add<CT>() where CT : struct
+		{
+			entity.Add<CT>();
+			return this;
+		}
+
+		public void Commit()
+		{
+			Spawner.OnSpawnEvent?.Invoke(entity);
+		}
+	}
+	
 	/// <summary>
 	/// 유니티에서 ECS 시스템의 Entity를 만들고, 제거하는 등 관리하는 함수.
 	/// </summary>
@@ -15,17 +36,17 @@ namespace Game.Service
 		/// <summary>
 		/// Entity를 만들고, 이벤트를 통해 ECS 시스템에 전달한다.
 		/// </summary>
-		public static void Spawn()
+		public static SpawnCommander Spawn(World world)
 		{
-			Entity result = new Entity();
-			OnSpawnEvent?.Invoke(result);
+			return new SpawnCommander(world);
 		}
 
 		//TODO: 유니티에서 제거할 ECS Entity를 인자로 넣을 방법이 있나?
 		// 아니면 게임 오브젝트를 인자로 넣으면 내부에서 Entity를 찾아주어야 하나?
 		public static void Despawn(Entity despawnEntity)
-        {
+		{
 			OnDespawnEvent?.Invoke(despawnEntity);
-        }
+			despawnEntity.Despawn();
+		}
 	}
 }
