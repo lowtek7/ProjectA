172ed5e0fc5ccdf3afb501e5cf471d17417d27d2
Hyukin Kwon <901po3@gmail.com>
2023-02-09 23:50:27 +0900

    Spawner에서 Despawn 하는 기능 추가

--- Assets/Scripts/Service/Spawner.cs
+++ Assets/Scripts/Service/Spawner.cs
@@ -1,20 +1,31 @@
-﻿using System;
+using System;
 using BlitzEcs;
 
 namespace Game.Service
-{
+{	
+	/// <summary>
+	/// 유니티에서 ECS 시스템의 Entity를 만들고, 제거하는 등 관리하는 함수.
+	/// </summary>
 	public static class Spawner
 	{
-		/// <summary>
-		/// 델리게이트 사용
-		/// </summary>
 		public static Action<Entity> OnSpawnEvent { get; set; }
 
+		public static Action<Entity> OnDespawnEvent { get; set; }
+
+		/// <summary>
+		/// Entity를 만들고, 이벤트를 통해 ECS 시스템에 전달한다.
+		/// </summary>
 		public static void Spawn()
 		{
-			// blah- 만들어짐 
 			Entity result = new Entity();
 			OnSpawnEvent?.Invoke(result);
 		}
+
+		//TODO: 유니티에서 제거할 ECS Entity를 인자로 넣을 방법이 있나?
+		// 아니면 게임 오브젝트를 인자로 넣으면 내부에서 Entity를 찾아주어야 하나?
+		public static void Despawn(Entity despawnEntity)
+        {
+			OnDespawnEvent?.Invoke(despawnEntity);
+        }
 	}
 }
