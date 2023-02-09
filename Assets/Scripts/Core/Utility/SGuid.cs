a4df9b2589a4a8f42777c2d5721f70ed15fed80e
Amber <lowtek7@gmail.com>
2023-02-10 02:31:12 +0900

    SGuid 구현

---
+++ Assets/Scripts/Core/Utility/SGuid.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core.Utility
+{
+	/// <summary>
+	/// 시리얼라이즈 가능한 Guid 구조체
+	/// </summary>
+	[Serializable]
+	public struct SGuid
+	{
+		[SerializeField]
+		private byte[] guidBytes;
+
+		private Guid guid;
+
+		public Guid Guid
+		{
+			get
+			{
+				if (guid == Guid.Empty)
+				{
+					guid = new Guid(guidBytes);
+				}
+				
+				return guid;
+			}
+			set
+			{
+				guidBytes = value.ToByteArray();
+				guid = value;
+			}
+		}
+
+		public SGuid(Guid guid)
+		{
+			this.guid = guid;
+			this.guidBytes = guid.ToByteArray();
+		}
+
+		public static SGuid Empty => new SGuid(Guid.Empty);
+	}
+}
