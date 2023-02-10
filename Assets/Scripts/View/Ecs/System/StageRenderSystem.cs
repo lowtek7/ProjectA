97761f0d6d782aa0248fdba58759c8e400cd4591
Amber <lowtek7@gmail.com>
2023-02-10 18:11:06 +0900

    스테이지 렌더링 코드 커밋

--- Assets/Scripts/View/Ecs/System/StageRenderSystem.cs
+++ Assets/Scripts/View/Ecs/System/StageRenderSystem.cs
@@ -15,11 +15,14 @@ namespace View.Ecs.System
 
 		private Query<StageRenderComponent> renderQuery;
 
+		private Query<UnitComponent, ZoneComponent> unitQuery;
+
 		public void Init(World world)
 		{
 			// 쿼리 조건에 ZoneComponent를 넣는것도 생각 해볼것
 			playerQuery = new Query<PlayerCameraComponent>(world);
 			renderQuery = new Query<StageRenderComponent>(world);
+			unitQuery = new Query<UnitComponent, ZoneComponent>(world);
 		}
 
 		public void Update(float deltaTime)
@@ -41,21 +44,49 @@ namespace View.Ecs.System
 				}
 			}
 
-			foreach (var entity in renderQuery)
+			if (StageRenderService.TryGetInstance(out var stageRenderService))
 			{
-				ref var stageRenderComponent = ref entity.Get<StageRenderComponent>();
-
-				// 현재 스테이지 Id와 스테이지 렌더 Component의 스테이지 Id가 다르므로 씬이동이 발생해야한다!
-				if (stageRenderComponent.StageId != currentStageId)
+				foreach (var entity in renderQuery)
 				{
-					// 스테이지 렌더 컴포넌트의 스테이지 Id를 변경
-					stageRenderComponent.StageId = currentStageId;
-					if (StageRenderService.TryGetInstance(out var stageRenderService))
+					ref var stageRenderComponent = ref entity.Get<StageRenderComponent>();
+
+					// 현재 스테이지 Id와 스테이지 렌더 Component의 스테이지 Id가 다르므로 씬이동이 발생해야한다!
+					if (stageRenderComponent.StageId != currentStageId)
 					{
+						// 스테이지 렌더 컴포넌트의 스테이지 Id를 변경
+						stageRenderComponent.StageId = currentStageId;
 						stageRenderService.StageTransition(currentStageId);
 					}
+					else if (!stageRenderService.IsLoading)
+					{
+						// 씬이동이 발생하지 않으면 모든 엔티티의 ZoneComponent를 조회하여 그려주거나 지워주면 된다.
+						unitQuery.Fetch();
+						foreach (var unitEntity in unitQuery)
+						{
+							// 해당 엔티티가 현재 스테이지와 일치하는지에 대한 검사
+							var zoneComponent = unitEntity.Get<ZoneComponent>();
+							if (zoneComponent.StageId == currentStageId)
+							{
+								// 만약 일치 한다면 render service에서 현재 그려주고 있는지에 대한 검사를 해야한다.
+								if (!stageRenderService.Contains(unitEntity.Id))
+								{
+									// 그려주고 있지 않다면 그려주게 만든다.
+									stageRenderService.StageEnterEvent(unitEntity);
+								}
+							}
+							else
+							{
+								// 스테이지와 일치하지 않는데 만약 그리고 있다면 스테이지에서 제외 시킨다.
+								if (stageRenderService.Contains(unitEntity.Id))
+								{
+									stageRenderService.StageExitEvent(unitEntity);
+								}
+							}
+						}
+					}
+
+					break;
 				}
-				break;
 			}
 		}
 	}
--- Assets/Scripts/View/Service/StageRenderService.cs
+++ Assets/Scripts/View/Service/StageRenderService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using BlitzEcs;
 using Game;
+using Game.Asset;
 using Game.Ecs.Component;
 using Game.Unit;
 using UnityEngine;
@@ -31,7 +33,23 @@ namespace View.Service
 
 		private World selfWorld = null;
 
-		private readonly List<UnitTemplate> loadedUnits = new List<UnitTemplate>();
+		private bool isLoading = false;
+
+		public bool IsLoading => isLoading;
+
+		/// <summary>
+		/// Key = Entity Id
+		/// Value = Unit Template
+		/// </summary>
+		private readonly Dictionary<int, UnitTemplate> loadedUnits = new ();
+
+		/// <summary>
+		/// 현재 스테이지에서 해당 엔티티 Id가 그려지고 있는지 검사
+		/// 함수 이름은 바꿀 필요가 있는듯
+		/// </summary>
+		/// <param name="entityId"></param>
+		/// <returns></returns>
+		public bool Contains(int entityId) => loadedUnits.ContainsKey(entityId);
 
 		private void Awake()
 		{
@@ -56,25 +74,144 @@ namespace View.Service
 		public void StageTransition(int stageId)
 		{
 			// 여기서 로드 되어있는 스테이지는 언로드하고 새로운 스테이지는 로드
+			if (!isLoading)
+			{
+				StartCoroutine(StageTransitionProcess(stageId));
+			}
+			else
+			{
+				Debug.LogError("StageTransition Failed.");
+			}
 		}
 
-		private IEnumerator StageTransitionProcess()
+		private IEnumerator StageTransitionProcess(int stageId)
 		{
-			// UI적으로 페이드 먹이거나 하는 것도 여기서 해줘야 함. (검은 화면으로 로딩하는 모습을 가리기 위해)
-			foreach (var unit in loadedUnits)
+			isLoading = true;
+			var assetFactory = AssetFactory.Instance;
+			if (assetFactory.TryGetAssetReader<UnitPrefabAssetModule>(out var reader) &&
+				reader is UnitPrefabAssetModule unitPrefabAssetModule)
 			{
-				unit.Disconnect();
+				// UI적으로 페이드 먹이거나 하는 것도 여기서 해줘야 함. (검은 화면으로 로딩하는 모습을 가리기 위해)
+				foreach (var keyValuePair in loadedUnits)
+				{
+					StageExitEventInternal(unitPrefabAssetModule, keyValuePair.Value);
+				}
+
+				loadedUnits.Clear();
+				yield return null;
+
+				// ZoneComponent를 지닌 엔티티들을 가져와야한다.
+				var query = new Query<UnitComponent, ZoneComponent>(selfWorld);
+				query.Fetch();
+
+				// 읽기전용으로 루프를 돌릴꺼기 때문에 query 내부 foreach를 이용 안한다
+				foreach (var entity in query)
+				{
+					var unitComponent = entity.Get<UnitComponent>();
+					var zoneComponent = entity.Get<ZoneComponent>();
+
+					// 해당 엔티티가 속한 StageId가 이동하려는 곳과 같은 경우 불러와 준다.
+					if (zoneComponent.StageId == stageId)
+					{
+						if (StageEnterEventInternal(unitPrefabAssetModule, unitComponent.SourceGuid, entity, out var unitTemplate))
+						{
+							loadedUnits.Add(entity.Id, unitTemplate);
+						}
+					}
+				}
+			}
+			else
+			{
+				Debug.LogError("StageTransitionProcess Failed! UnitPrefabAssetModule is unloaded");
 			}
 
 			yield return null;
+			isLoading = false;
+		}
+
+		private bool StageEnterEventInternal(UnitPrefabAssetModule unitPrefabAssetModule, Guid sourceGuid, Entity entity, out UnitTemplate unitTemplate)
+		{
+			unitTemplate = null;
+			if (unitPrefabAssetModule.TrySpawn(sourceGuid, out var result))
+			{
+				unitTemplate = result;
+				result.Connect(entity);
+				return true;
+			}
+			
+			return false;
+		}
+
+		/// <summary>
+		/// 스테이지 도중에 특정 엔티티가 입장 할 경우 이 함수를 호출 해준다.
+		/// </summary>
+		/// <param name="entity"></param>
+		public void StageEnterEvent(Entity entity)
+		{
+			if (isLoading)
+			{
+				Debug.LogWarning("StageEnterEvent Failed. StageRenderService is loading...");
+				return;
+			}
+
+			var entityId = entity.Id;
+
+			if (loadedUnits.ContainsKey(entityId))
+			{
+				Debug.LogWarning("Entity is already loadedUnits");
+				return;
+			}
+
+			if (!entity.Has<UnitComponent>())
+			{
+				Debug.LogError("Entity does not have UnitComponent.");
+				return;
+			}
 
 			var assetFactory = AssetFactory.Instance;
-			var query = new Query<UnitComponent>(selfWorld);
-			query.Fetch();
-
-			foreach (var entity in query)
+			if (assetFactory.TryGetAssetReader<UnitPrefabAssetModule>(out var reader) &&
+				reader is UnitPrefabAssetModule unitPrefabAssetModule)
 			{
-				
+				if (StageEnterEventInternal(unitPrefabAssetModule,
+						entity.Get<UnitComponent>().SourceGuid,
+						entity,
+						out var unitTemplate))
+				{
+					loadedUnits.Add(entityId, unitTemplate);
+				}
+			}
+		}
+
+		private void StageExitEventInternal(UnitPrefabAssetModule unitPrefabAssetModule, UnitTemplate unitTemplate)
+		{
+			// 무조건 Disconnect를 선행으로 작업해주어야 한다.
+			unitTemplate.Disconnect();
+			unitPrefabAssetModule.Despawn(unitTemplate);
+		}
+
+		/// <summary>
+		/// 스테이지 도중에 엔티티가 사라질 경우 이 함수를 호출 해준다.
+		/// </summary>
+		/// <param name="entity"></param>
+		public void StageExitEvent(Entity entity)
+		{
+			if (isLoading)
+			{
+				Debug.LogWarning("StageExitEvent Failed. StageRenderService is loading...");
+				return;
+			}
+
+			var entityId = entity.Id;
+
+			if (loadedUnits.TryGetValue(entityId, out var unitTemplate))
+			{
+				var assetFactory = AssetFactory.Instance;
+				if (assetFactory.TryGetAssetReader<UnitPrefabAssetModule>(out var reader) &&
+					reader is UnitPrefabAssetModule unitPrefabAssetModule)
+				{
+					StageExitEventInternal(unitPrefabAssetModule, unitTemplate);
+					loadedUnits.Remove(entityId);
+				}
 			}
 		}
 	}
