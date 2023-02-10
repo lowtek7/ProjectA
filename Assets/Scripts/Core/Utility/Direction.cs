65f2c3b9f44357a5c24864139cf424444c7bf829
xiture9640 <xiture9640@naver.com>
2023-02-11 03:55:33 +0900

    Direction Enum 추가

---
+++ Assets/Scripts/Core/Utility/Direction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Core.Utility
+{
+	[Flags]
+	public enum Direction
+	{
+		None = 0,
+		Up = 1 << 0,
+		Right = 1 << 1,
+		Down = 1 << 2,
+		Left = 1 << 3,
+		All = Up | Right | Down | Left,
+	}
+}
