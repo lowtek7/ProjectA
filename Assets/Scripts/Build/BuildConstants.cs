ee0eb3d32f94d3af112e8d83f04424778ae73929
Amber <lowtek7@gmail.com>
2023-02-17 21:21:53 +0900

    WorldDataPath 중복으로 두던것 수정 (BuildConstants에서 제거)

--- Assets/Scripts/Build/BuildConstants.cs
+++ Assets/Scripts/Build/BuildConstants.cs
@@ -6,10 +6,5 @@
 	public static class BuildConstants
 	{
 		public static string StagePrefabPath => "Assets/GameAsset/Prefab/StagePrefab/";
-
-		/// <summary>
-		/// World 데이터의 경로
-		/// </summary>
-		public static string WorldDataPath => "Assets/StaticData/World.json";
 	}
 }
--- Assets/Scripts/Service/SaveLoad/SaveLoadConstants.cs
+++ Assets/Scripts/Service/SaveLoad/SaveLoadConstants.cs
@@ -4,8 +4,6 @@
 	{
 		/// <summary>
 		/// World 데이터의 경로
-		/// 현재 BuildConstants에도 존재하므로 중복 데이터...
-		/// 어딘가에 하나만 두는게 좋을듯
 		/// </summary>
 		public static string WorldDataPath => "Assets/StaticData/World.json";
 	}
