680bef591850f1cc3ae988cdfb8d5ed5656e851c
Amber <lowtek7@gmail.com>
2023-05-14 02:04:43 +0900

    파티셔닝에서 겹치는 엔티티들을 가져올 수 있는 함수 추가

--- Assets/Scripts/UnityService/Stage/UnityStagePartitionService.cs
+++ Assets/Scripts/UnityService/Stage/UnityStagePartitionService.cs
@@ -16,7 +16,11 @@ namespace UnityService.Stage
 	{
 		private readonly Dictionary<Guid, KdTree<float, HashSet<int>>> _treeMap = new Dictionary<Guid, KdTree<float, HashSet<int>>>();
 
+		private World _world;
+		
 		private Query<TransformComponent, StageSpecComponent> _query;
+
+		private float[] _arrayBuffer;
 
 		/// <summary>
 		/// KD-Tree에서 사용 할 차원
@@ -25,15 +29,15 @@ namespace UnityService.Stage
 
 		public void Init(World world)
 		{
+			_world = world;
 			_query = new Query<TransformComponent, StageSpecComponent>(world);
+			_arrayBuffer = new float[] { 0, 0, 0 };
+
 			_treeMap.Clear();
 		}
 
 		void IStagePartitionService.Fetch()
 		{
-			// 메모리 할당을 최대한 피하기 위해 buffer 할당
-			var arrayBuffer = new float[] { 0, 0, 0 };
-
 			foreach (var tree in _treeMap.Values)
 			{
 				tree.Clear();
@@ -55,17 +59,17 @@ namespace UnityService.Stage
 				var tree = _treeMap[stageGuid];
 				var pos = transformComponent.Position;
 
-				PosToArrayBuffer(pos, ref arrayBuffer);
+				PosToArrayBuffer(pos, ref _arrayBuffer);
 
 				{
-					if (tree.TryFindValueAt(arrayBuffer, out var idSet))
+					if (tree.TryFindValueAt(_arrayBuffer, out var idSet))
 					{
 						idSet.Add(id);
 					}
 					else
 					{
 						idSet = new HashSet<int> { id };
-						tree.Add(arrayBuffer.ToArray(), idSet);
+						tree.Add(_arrayBuffer.ToArray(), idSet);
 					}
 				}
 
@@ -78,16 +82,16 @@ namespace UnityService.Stage
 					// 총 8개의 점을 tree에 기록 해야한다.
 					foreach (var point in points)
 					{
-						PosToArrayBuffer(point, ref arrayBuffer);
+						PosToArrayBuffer(point, ref _arrayBuffer);
 
-						if (tree.TryFindValueAt(arrayBuffer, out var idSet))
+						if (tree.TryFindValueAt(_arrayBuffer, out var idSet))
 						{
 							idSet.Add(id);
 						}
 						else
 						{
 							idSet = new HashSet<int> { id };
-							tree.Add(arrayBuffer.ToArray(), idSet);
+							tree.Add(_arrayBuffer.ToArray(), idSet);
 						}
 					}
 				}
@@ -110,6 +114,52 @@ namespace UnityService.Stage
 			array[0] = pos.x;
 			array[1] = pos.y;
 			array[2] = pos.z;
+		}
+
+		/// <summary>
+		/// 해당 엔티티와 겹치는 엔티티들을 모조리 가져오는 함수.
+		/// 미리 할당된 list 객체를 넣어주어야 한다.
+		/// </summary>
+		/// <param name="entity"></param>
+		/// <param name="overlappedEntities">미리 할당된 list 객체. 겹친 엔티티들을 내뱉는다</param>
+		public void GetBoundsOverlappedEntities(Entity entity, ref List<Entity> overlappedEntities)
+		{
+			var idSet = new HashSet<Entity>();
+			overlappedEntities.Clear();
+
+			if (entity.Has<StageSpecComponent>() &&
+				entity.Has<BoundsComponent>() &&
+				entity.Has<TransformComponent>())
+			{
+				var stageGuid = entity.Get<StageSpecComponent>().StageGuid;
+				var pos = entity.Get<TransformComponent>().Position;
+				var bounds = entity.Get<BoundsComponent>().GetBounds(pos);
+
+				if (_treeMap.TryGetValue(stageGuid, out var tree))
+				{
+					// 모든 사이즈 중 가장 큰 사이즈의 값을 radius로 사용 해야한다.
+					var radius = Mathf.Max(Mathf.Max(Mathf.Max(1, bounds.size.x), bounds.size.y), bounds.size.z);
+
+					// 버퍼에 pos을 복사
+					PosToArrayBuffer(pos, ref _arrayBuffer);
+
+					var results = tree.RadialSearch(_arrayBuffer, radius);
+
+					foreach (var node in results)
+					{
+						// 해당 점들과 비교한다.
+						if (bounds.Contains(new Vector3(node.Point[0], node.Point[1], node.Point[2])))
+						{
+							foreach (var id in node.Value)
+							{
+								idSet.Add(new Entity(_world, id));
+							}
+						}
+					}
+				}
+			}
+
+			overlappedEntities.AddRange(idSet);
 		}
 	}
 }
