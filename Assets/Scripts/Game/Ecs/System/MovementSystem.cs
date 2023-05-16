e2eb835b2f9a9edb1b0c87aefe2f899a9a09df52
Amber <lowtek7@gmail.com>
2023-05-17 02:51:01 +0900

    movement system의 foreach를 변경

--- Assets/Scripts/Game/Ecs/System/MovementSystem.cs
+++ Assets/Scripts/Game/Ecs/System/MovementSystem.cs
@@ -9,28 +9,33 @@ namespace Game.Ecs.System
 	{
 		private Query<TransformComponent, MovementComponent> _moveQuery;
 
+		private BlitzEcs.World _world;
+
 		public Order Order => Order.Normal;
 
 		public void Init(BlitzEcs.World world)
 		{
 			_moveQuery = new Query<TransformComponent, MovementComponent>(world);
+			_world = world;
 		}
 
 		public void Update(float deltaTime)
 		{
-			_moveQuery.ForEach((Entity entity,
-				ref TransformComponent transformComponent,
-				ref MovementComponent movementComponent) =>
+			foreach (var entity in _moveQuery)
 			{
+				ref var transformComponent = ref entity.Get<TransformComponent>();
+				var movementComponent = entity.Get<MovementComponent>();
+
 				// MoveDir가 zero가 아니라면 계산해줌
 				if (movementComponent.MoveDir != Vector3.zero)
 				{
 					// 여기서 normalize하는것이 과연 올바른것일까?
 					var dir = movementComponent.MoveDir;
 					var dist = movementComponent.MoveSpeed * deltaTime;
+
 					transformComponent.Position += (dir * dist);
 				}
-			});
+			}
 		}
 
 		public void LateUpdate(float deltaTime)
