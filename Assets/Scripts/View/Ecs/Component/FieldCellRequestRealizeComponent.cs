acdc6a6fe868e8c930c5058299c43f250b5c96f5
xiture9640 <xiture9640@naver.com>
2023-05-21 03:39:12 +0900

    필드 셀 가상화/실체화 요청 컴포넌트 추가

---
+++ Assets/Scripts/View/Ecs/Component/FieldCellRequestRealizeComponent.cs
@@ -0,0 +1,15 @@
+﻿using Core.Unity;
+
+namespace View.Ecs.Component
+{
+	/// <summary>
+	/// 필드 셀의 GameObject화 요청
+	/// </summary>
+	public struct FieldCellRequestRealizeComponent : IComponent
+	{
+		public IComponent Clone()
+		{
+			return new FieldCellRequestRealizeComponent();
+		}
+	}
+}
---
+++ Assets/Scripts/View/Ecs/Component/FieldCellRequestVirtualizeComponent.cs
@@ -0,0 +1,15 @@
+﻿using Core.Unity;
+
+namespace View.Ecs.Component
+{
+	/// <summary>
+	/// 필드 셀의 Virtualize 요청
+	/// </summary>
+	public struct FieldCellRequestVirtualizeComponent : IComponent
+	{
+		public IComponent Clone()
+		{
+			return new FieldCellRequestVirtualizeComponent();
+		}
+	}
+}
