7f49affbf78c356737c850e1cc32bf5c8d46c774
xiture9640 <xiture9640@naver.com>
2023-06-25 04:18:35 +0900

    복셀 메시 생성을 위한 텍스쳐 패킹 기능 추가

---
+++ Assets/Scripts/Game/Editor/Texture/TexturePacker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Game.Editor.Texture
+{
+	[CreateAssetMenu(menuName = "TexturePacking/TexturePacker", fileName = "NewTexturePacker")]
+	public class TexturePacker : ScriptableObject
+	{
+		public List<Texture2D> textures;
+		public string filePath = "Assets/GameAsset";
+		public string fileName = "Texture";
+	}
+}
---
+++ Assets/Scripts/Game/Editor/Texture/TexturePackerEditor.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using Application = UnityEngine.Device.Application;
+using IO = System.IO;
+
+namespace Game.Editor.Texture
+{
+	[CustomEditor(typeof(TexturePacker))]
+	public class TexturePackerEditor : UnityEditor.Editor
+	{
+		private TexturePacker _packer;
+
+		private void OnEnable()
+		{
+			_packer = target as TexturePacker;
+		}
+
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+
+			if (Application.isPlaying)
+				return;
+
+			if (GUILayout.Button("Generate"))
+			{
+				GenerateTexture();
+			}
+
+			serializedObject.ApplyModifiedProperties();
+		}
+
+		private void GenerateTexture()
+		{
+			var pixelSize = 1 << 8;
+
+			var newTexture = new Texture2D(pixelSize, pixelSize);
+
+			var drawPos = Vector2Int.zero;
+
+			foreach (var texture in _packer.textures)
+			{
+				var width = texture.width;
+				var height = texture.height;
+
+				if (drawPos.x + width > newTexture.width)
+				{
+					// FIXME : 빈 공간을 찾아 떠나야 함
+					drawPos = new Vector2Int(0, drawPos.y + height);
+				}
+
+				for (int x = 0; x < width; x++)
+				{
+					for (int y = 0; y < height; y++)
+					{
+						var pixel = texture.GetPixel(x, y);
+
+						newTexture.SetPixel(drawPos.x + x, drawPos.y + y, pixel);
+					}
+				}
+
+				drawPos.x += width;
+			}
+
+			newTexture.Apply();
+
+			#region FileIO
+
+			var dirPath = $"{_packer.filePath}";
+			var fileName = $"{dirPath}/{_packer.fileName}.png";
+
+			if (!IO.Directory.Exists(dirPath))
+			{
+				IO.Directory.CreateDirectory(dirPath);
+			}
+
+			var pngBytes = newTexture.EncodeToPNG();
+
+			IO.File.WriteAllBytes(fileName, pngBytes);
+
+			AssetDatabase.ImportAsset(fileName, ImportAssetOptions.ForceUpdate);
+			AssetDatabase.SaveAssets();
+			AssetDatabase.Refresh();
+
+			#endregion
+
+			var newTextureAsset = AssetDatabase.LoadAssetAtPath(fileName, typeof(Texture2D)) as Texture2D;
+
+			if (AssetDatabase.IsMainAsset(newTextureAsset))
+			{
+				var originImporter = AssetImporter.GetAtPath(
+					AssetDatabase.GetAssetPath(_packer.textures[0])) as UnityEditor.TextureImporter;
+
+				var originSetting = new TextureImporterSettings();
+				originImporter.ReadTextureSettings(originSetting);
+
+				var originPlatformSetting = originImporter.GetDefaultPlatformTextureSettings();
+
+				var newImporter = AssetImporter.GetAtPath(fileName) as UnityEditor.TextureImporter;
+				var newSetting = new TextureImporterSettings();
+				var newPlatformSetting = new TextureImporterPlatformSettings();
+
+				originSetting.CopyTo(newSetting);
+				originPlatformSetting.CopyTo(newPlatformSetting);
+
+				newSetting.readable = false;
+				newPlatformSetting.maxTextureSize = pixelSize;
+
+				newImporter.SetTextureSettings(newSetting);
+				newImporter.SetPlatformTextureSettings(newPlatformSetting);
+
+				if (AssetDatabase.WriteImportSettingsIfDirty(newImporter.assetPath))
+				{
+					newImporter.SaveAndReimport();
+				}
+			}
+		}
+	}
+}
