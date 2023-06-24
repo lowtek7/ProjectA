d4ad93a1bb2365eae445082cdab916e6df3646ce
xiture9640 <xiture9640@naver.com>
2023-06-24 22:08:20 +0900

    기존 Chunk 시스템 만들던 것 삭제

--- Assets/Scripts/Game/World/Constants.cs
+++ Assets/Scripts/Game/World/Constants.cs
@@ -8,25 +8,5 @@ namespace Game.World
 	public static class Constants
 	{
 		public static Guid UnknownStageGuid => Guid.Empty;
-
-		/// <summary>
-		/// 청크의 실제 크기 (16 x 16)
-		/// </summary>
-		public static byte ChunkUnitSize => 1 << 4;
-
-		/// <summary>
-		/// 맵 내의 전체 청크 Extents (1024 x 1024)
-		/// </summary>
-		public static int TotalChunkExtents => 1 << 10;
-
-		/// <summary>
-		/// 맵 내의 전체 청크 개수 (2048 x 2048)
-		/// </summary>
-		public static int TotalChunkSize => TotalChunkExtents << 1;
-
-		/// <summary>
-		/// 플레이어 기준 x,y 합산 거리가 16 * 8 이하인 청크 내부 셀만 실제 엔티티로 업데이트할 것임
-		/// </summary>
-		public static short UpdatableChunkExtents => 1 << 3;
 	}
 }
