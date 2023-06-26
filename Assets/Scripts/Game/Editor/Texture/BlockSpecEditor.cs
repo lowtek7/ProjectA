2079f821160e82bbe71eafb38282d168a9343377
xiture9640 <xiture9640@naver.com>
2023-06-27 01:04:22 +0900

    BlockSpec 왜안되는지 확인하기 위해 임시로 추가

---
+++ Assets/Scripts/Game/Editor/Texture/BlockSpecEditor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Game.Editor.Texture
+{
+	[CustomPropertyDrawer(typeof(BlockSpec))]
+	public class BlockSpecDrawer : UnityEditor.PropertyDrawer
+	{
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+		{
+			return (16 + 4) * (1 + 6);
+		}
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			EditorGUI.BeginProperty(position, label, property);
+
+			var prevPos = new Rect(position.position.x, position.position.y + 2, position.width, 16);
+			EditorGUI.PropertyField(prevPos, property.FindPropertyRelative(nameof(BlockSpec.name)));
+
+			var textureArrayProp = property.FindPropertyRelative(nameof(BlockSpec.textures));
+
+			for (int i = 0; i < textureArrayProp.arraySize; i++)
+			{
+				prevPos = new Rect(position.x, prevPos.yMax + 4, position.width, 16);
+
+				EditorGUI.PropertyField(prevPos, textureArrayProp.GetArrayElementAtIndex(i));
+			}
+
+			EditorGUI.EndProperty();
+		}
+	}
+}
---
+++ Assets/Scripts/Game/Editor/Texture/BlockSpecHolder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Game.Editor.Texture
+{
+	[Serializable]
+	public class BlockSpec
+	{
+		public string name = "new_block";
+		public Texture2D[] textures = new Texture2D[6]
+		{
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+		};
+	}
+
+	[CreateAssetMenu(menuName = "BlockData/Texture", fileName = "NewBlockSpecHolder")]
+	public class BlockSpecHolder : ScriptableObject
+	{
+		public List<BlockSpec> blockSpecs = new();
+
+		private Dictionary<string, BlockSpec> _nameToSpec;
+
+		public Dictionary<string, BlockSpec> NameToSpec => _nameToSpec ??= blockSpecs.ToDictionary(spec => spec.name);
+	}
+}
