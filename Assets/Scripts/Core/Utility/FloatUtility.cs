6761cf84234ae5985256d13dec0426c831101f27
amber <lowtek7@gmail.com>
2023-06-28 00:55:16 +0900

    float utility 함수 몇개 추가

--- Assets/Scripts/Core/Utility/FloatUtility.cs
+++ Assets/Scripts/Core/Utility/FloatUtility.cs
@@ -5,5 +5,15 @@ namespace Core.Utility
 	public static class FloatUtility
 	{
 		public static float Epsilon => 0.001f;
+
+		public static bool IsAlmostZero(this float value)
+		{
+			return Mathf.Abs(value) <= Epsilon;
+		}
+
+		public static bool IsAlmostCloseTo(this float value, float target)
+		{
+			return Mathf.Abs(value - target) <= Epsilon;
+		}
 	}
 }
