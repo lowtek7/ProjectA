407f40f3016ee71641c62e61326b8c39392f8d87
ArcticFox <jimmy9378@naver.com>
2023-07-14 01:53:50 +0900

    Vector 유틸리티 추가

---
+++ Assets/Scripts/Core/Utility/VectorUtility.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Core.Utility
+{
+	public static class VectorUtility
+	{
+		public static Vector3 GetX0Z(this Vector3 value, float y = 0)
+		{
+			return new Vector3(value.x, y, value.z);
+		}
+	}
+}
