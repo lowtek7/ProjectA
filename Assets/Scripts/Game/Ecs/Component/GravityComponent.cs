735ca42d0d77f9900159b5ea7b7cff96217fb9fc
ArcticFox <jimmy9378@naver.com>
2023-07-14 01:54:36 +0900

    임시 중력 시스템 및 컴포넌트 추가

---
+++ Assets/Scripts/Game/Ecs/Component/GravityComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Core.Unity;
+
+namespace Game.Ecs.Component
+{
+	[Serializable]
+	public struct GravityComponent : IComponent
+	{
+		public IComponent Clone()
+		{
+			return new GravityComponent();
+		}
+	}
+}
---
+++ Assets/Scripts/Game/Ecs/System/GravitySystem.cs
@@ -0,0 +1,44 @@
+﻿using BlitzEcs;
+using Core.Unity;
+using Core.Utility;
+using Game.Ecs.Component;
+using Service;
+using Service.Command;
+using UnityEngine;
+
+namespace Game.Ecs.System
+{
+	public class GravitySystem : ISystem
+	{
+		private Query<TransformComponent, GravityComponent> _gravityQuery;
+
+		public void Init(BlitzEcs.World world)
+		{
+			_gravityQuery = new Query<TransformComponent, GravityComponent>(world);
+		}
+
+		public void Update(float deltaTime)
+		{
+			foreach (var entity in _gravityQuery)
+			{
+				ref var transformComponent = ref entity.Get<TransformComponent>();
+
+				var dist = (-1.0f * deltaTime);
+
+				// 임시로 0까지만 중력 받도록 함
+				if (transformComponent.Position.y + dist > 0.0f)
+				{
+					transformComponent.Position += Vector3.up * dist;
+				}
+				else
+				{
+					transformComponent.Position = transformComponent.Position.GetX0Z();
+				}
+			}
+		}
+
+		public void LateUpdate(float deltaTime)
+		{
+		}
+	}
+}
