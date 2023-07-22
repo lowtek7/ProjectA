e50dcf3f2b66fa1fc9be9142b26619401e513163
xiture9640 <xiture9640@naver.com>
2023-07-22 15:12:37 +0900

    EntityDebuggerWindow에 Query 기능 추가

--- Assets/Scripts/RuntimeDebugger/EntityDebuggerWindow.cs
+++ Assets/Scripts/RuntimeDebugger/EntityDebuggerWindow.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using BlitzEcs;
+using Core.Unity;
+using Core.Utility;
 using Game.Ecs.Component;
+using MackySoft.SerializeReferenceExtensions.Editor;
 using Service;
 using Service.World;
 using UnityEditor;
+using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 
 namespace RuntimeDebugger
@@ -15,13 +21,88 @@ namespace RuntimeDebugger
 
 		private readonly Dictionary<int, Dictionary<int, bool>> foldMap = new Dictionary<int, Dictionary<int, bool>>();
 
+		private readonly HashSet<Type> _includedComponents = new();
+		private readonly HashSet<Type> _excludedComponents = new();
+
+		private readonly List<Type> _removedByFilterBuffer = new();
+
 		private void OnGUI()
 		{
 			if (ServiceManager.TryGetService(out IWorldGetterService worldGetterService))
 			{
 				var world = worldGetterService.World;
+
+				if (world == null)
+				{
+					return;
+				}
+
 				var entityCount = world.EntityCount;
 				var componentCount = world.ComponentCount;
+
+				using (new GUILayout.HorizontalScope())
+				{
+					GUILayout.Label("Included");
+
+					if (GUILayout.Button("Add"))
+					{
+						OnAdd(_includedComponents);
+					}
+				}
+
+				foreach (var type in _includedComponents)
+				{
+					using (new GUILayout.HorizontalScope())
+					{
+						GUILayout.Label(type.FullName);
+
+						if (GUILayout.Button(" - "))
+						{
+							_removedByFilterBuffer.Add(type);
+						}
+					}
+				}
+
+				foreach (var type in _removedByFilterBuffer)
+				{
+					_includedComponents.Remove(type);
+				}
+
+				_removedByFilterBuffer.Clear();
+
+				EditorGUILayout.Space();
+
+				using (new GUILayout.HorizontalScope())
+				{
+					GUILayout.Label("Excluded");
+
+					if (GUILayout.Button("Add"))
+					{
+						OnAdd(_excludedComponents);
+					}
+				}
+
+				foreach (var type in _excludedComponents)
+				{
+					using (new GUILayout.HorizontalScope())
+					{
+						GUILayout.Label(type.FullName);
+
+						if (GUILayout.Button(" - "))
+						{
+							_removedByFilterBuffer.Add(type);
+						}
+					}
+				}
+
+				foreach (var type in _removedByFilterBuffer)
+				{
+					_excludedComponents.Remove(type);
+				}
+
+				_removedByFilterBuffer.Clear();
+
+				EditorGUILayout.Space();
 
 				using (var scope = new EditorGUILayout.ScrollViewScope(scrollPos))
 				{
@@ -34,6 +115,8 @@ namespace RuntimeDebugger
 							break;
 						}
 
+						index++;
+
 						if (!foldMap.TryGetValue(entityId, out var componentFoldMap))
 						{
 							componentFoldMap = new Dictionary<int, bool>();
@@ -41,6 +124,37 @@ namespace RuntimeDebugger
 						}
 
 						var entity = new Entity(world, entityId);
+
+						if (_includedComponents.Count > 0 || _excludedComponents.Count > 0)
+						{
+							var needShow = true;
+
+							var includedCount = 0;
+
+							for (int i = 0; i < componentCount; i++)
+							{
+								if (world.TryGetIComponentPool(i, out var pool) && pool.Contains(entityId))
+								{
+									var componentType = pool.ComponentType;
+
+									if (_excludedComponents.Contains(componentType))
+									{
+										needShow = false;
+										break;
+									}
+
+									if (_includedComponents.Contains(componentType))
+									{
+										includedCount++;
+									}
+								}
+							}
+
+							if (!needShow || includedCount < _includedComponents.Count)
+							{
+								continue;
+							}
+						}
 
 						if (entity.Has<NameComponent>())
 						{
@@ -88,8 +202,6 @@ namespace RuntimeDebugger
 						}
 
 						EditorGUI.indentLevel--;
-
-						++index;
 					}
 
 					scrollPos = scope.scrollPosition;
@@ -108,6 +220,29 @@ namespace RuntimeDebugger
 
 			window.titleContent = new GUIContent("Entity Debugger");
 			window.Show(true);
+		}
+
+		private void OnAdd(HashSet<Type> target)
+		{
+			var componentTypes = TypeUtility.GetTypesWithInterface(typeof(IComponent)).ToList();
+
+			foreach (var type in target)
+			{
+				componentTypes.Remove(type);
+			}
+
+			var state = new AdvancedDropdownState();
+			var popup = new AdvancedTypePopup(componentTypes,
+				13,
+				state);
+
+			popup.SetTypes(componentTypes);
+			popup.Show(new Rect(0, 0, 400, 400));
+
+			popup.OnItemSelected += (item) =>
+			{
+				target.Add(item.Type);
+			};
 		}
 	}
 }
